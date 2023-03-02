b08cb592ed88dd9b21583476ff4956e4acadb719
Sima Tarverdyan <102418053+twers1@users.noreply.github.com>
2023-03-02 15:48:16 +0300

    Add files via upload

---
+++ ОС/helloworld.java
@@ -0,0 +1,13 @@
+class HelloWorld {
+    public static void 
+    main(String[] args) {
+        int a =4;
+        double d=2.5;
+        boolean b=true;
+        char c=69;
+        System.out.print(c);
+        
+    }
+    
+}
+// сделали сами!

93f79686eba7a7455dfe27d260fd286566c5e7aa
Sima Tarverdyan <102418053+twers1@users.noreply.github.com>
2023-04-27 15:09:39 +0300

    add project

---
+++ Архитектура аппаратных средств | Технология разработки ПО/laba/project.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Runtime.CompilerServices;
+
+// Проект
+class Program
+{
+    static void Main(string[] args)
+    {
+        Car car2 = new Car(130, new Engine(25.5, "Лучший двигатель"));
+        car2.Print();
+
+
+        Car[] cars = new Car[2];
+        cars[0] = new Car(140, new Engine(15.5, "Плохой двигатель"));
+        cars[1] = new Car(300, new Engine(200, "Лучший двигатель"));
+        Car.getFastestCar(cars);
+    }
+
+}
+
+
+// Класс двигатель 
+class Engine
+{
+    public double power { get; set; }
+    public string name { get; set; }
+
+    public Engine(double power, string name)
+    {
+        this.power = power;
+        this.name = name;
+    }
+}
+// Enum для цвета машины
+enum Color
+{
+    Black,
+    White,
+    Red,
+    Gray,
+    Blue
+}
+
+// Класс машина
+class Car
+{
+    // Чтобы сделать так, чтобы автомобиль не мог изменить массу - надо убрать сеттер. 
+    public string nameCar { get; set; }
+    public int massCar { get;  }
+
+    public Color colorCar { get; set; }
+    public int speedCar { get; set; }
+    public Engine engineCar { get; set; }
+
+    public Car(int speedCar, Engine engineCar)
+    {
+        this.speedCar = speedCar;
+        this.engineCar = engineCar;
+    }
+
+    // Метод, ищущий машину с максимальной скоростью
+    public static Car getFastestCar(Car[] array)
+    {
+        //Console.WriteLine("Введите какое количество машин будет в вашем массиве: ");
+        //int n = Convert.ToInt32(Console.ReadLine());
+        Car[] cars = new Car[2];
+        cars[0] = new Car(140, new Engine(15.5, "Плохой двигатель"));
+        cars[1] = new Car(300, new Engine(200, "Лучший двигатель"));
+        Car fasterCar = cars[0];
+        int maxSpeed = 0;
+        foreach (Car car in cars)
+        {
+            if(car.speedCar > maxSpeed)
+            {
+                maxSpeed = car.speedCar;
+            }
+        }
+        Console.WriteLine("Максимальная скорость: " + maxSpeed);
+        return fasterCar;
+        
+
+    }
+
+    // Метод, который выводит всю информацию о машине
+    public void Print()
+    {
+        Console.WriteLine("Бренд машины" + nameCar);
+        Console.WriteLine("Текущая скорость: "+ speedCar);
+        Console.WriteLine("Цвет: "+colorCar);
+        Console.WriteLine("Мощность двигателя: "+engineCar);
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+

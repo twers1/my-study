8c59332e55129b5448bf1668f6fe13aae651bfb7
Sima Tarverdyan <102418053+twers1@users.noreply.github.com>
2023-05-14 21:09:27 +0300

    add: DBLaba

---
+++ Архитектура аппаратных средств | Технология разработки ПО/laba/DBlaba/ImportService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DBlaba
+{
+    public class ImportService
+    {
+        SqlConnectionStringBuilder connectionStringBuilder= new SqlConnectionStringBuilder(@"Data Sourse = C:\Users\Admin\source\repos\DBlaba\DBlaba\productDB.mdf");
+        public ImportService(string[] dirPaths, string fileExt)
+        {
+            int n = dirPaths.Length;
+
+        }
+
+       
+    }
+
+    public class ImportTask : Runnable
+    {
+        public ImportTask(string dirPath, string fileExt)
+        {
+
+        }
+    }
+}
